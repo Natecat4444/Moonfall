bebca5cd1b7b1e268f56e5d6f55832ca0a3697be
nathankaufman <nlk7665@rit.edu>
2022-02-07 10:14:55 -0500

    Show constructor getters and setters

--- src/main/java/com/cognixia/jump/moonfall/Show.java
+++ src/main/java/com/cognixia/jump/moonfall/Show.java
@@ -2,13 +2,33 @@ package com.cognixia.jump.moonfall;
 
 public class Show {
 
-		private String Title = "";
+		private String Title;
 		private static int ShowID;
 		
+		public Show() {
+			
+		}
 		
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public Show(String Title) {
+			this.Title = Title;
+		}
 
-	}
+		public String getTitle() {
+			return Title;
+		}
+
+		public void setTitle(String title) {
+			Title = title;
+		}
+
+		public static int getShowID() {
+			return ShowID;
+		}
+
+		public static void setShowID(int showID) {
+			ShowID = showID;
+		}
+		
+		
 
 }

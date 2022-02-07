5f0f121cdae70a4ece618e58ddb01cdf1adfb1ce
nathankaufman <nlk7665@rit.edu>
2022-02-07 10:39:07 -0500

    Show fetch method

--- src/main/java/com/cognixia/jump/moonfall/Show.java
+++ src/main/java/com/cognixia/jump/moonfall/Show.java
@@ -1,5 +1,10 @@
 package com.cognixia.jump.moonfall;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Show {
 
 		private String Title;
@@ -29,6 +34,22 @@ public class Show {
 			ShowID = showID;
 		}
 		
+		public void fetch() {
+			Connection conn = ConnectionManager.getConnection();
+			PreparedStatement stmt;
+			ResultSet rs;
+			
+			try {
+				stmt = conn.prepareStatement("SELECT ShowID from Shows WHERE Title=?");
+				stmt.setString(1, Title);
+				rs = stmt.executeQuery();
+				if(rs.next()) {
+					this.ShowID = rs.getInt("ShowID");
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		
 
 }
